6afe92bbab9e019cee9fcdc3caabd3d4697ec7cf
jeounpar <cdex6531@ajou.ac.kr>
2022-08-01 16:51:52 +0900

    first commit

---
+++ .eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  root: true,
+  extends: [
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  parserOptions: {
+    parser: '@typescript-eslint/parser',
+  },
+};
