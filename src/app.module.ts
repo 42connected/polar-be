efeb8c808ab470d2d5f8e1dde43a5168bd9d6d05
kanghyki <kanghyki@gmail.com>
2022-08-03 18:28:08 +0900

    POL-25 Feat: API Routing

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,12 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmConfigService } from './config/typeorm.config';
 import { V1Module } from './v1/v1.module';
+import { RouterModule } from '@nestjs/core';
+import { MentorsModule } from './v1/mentors/mentors.module';
+import { ReportsModule } from './v1/reports/reports.module';
+import { KeywordsModule } from './v1/keywords/keywords.module';
+import { CadetsModule } from './v1/cadets/cadets.module';
+import { BocalsModule } from './v1/bocals/bocals.module';
 
 @Module({
   imports: [
@@ -15,6 +21,34 @@ import { V1Module } from './v1/v1.module';
       useClass: TypeOrmConfigService,
     }),
     V1Module,
+    RouterModule.register([
+      {
+        path: 'api/v1',
+        module: V1Module,
+        children: [
+          {
+            path: 'cadets',
+            module: CadetsModule,
+          },
+          {
+            path: 'mentors',
+            module: MentorsModule,
+          },
+          {
+            path: 'bocals',
+            module: BocalsModule,
+          },
+          {
+            path: 'keywords',
+            module: KeywordsModule,
+          },
+          {
+            path: 'reports',
+            module: ReportsModule,
+          },
+        ],
+      },
+    ]),
   ],
   controllers: [AppController],
   providers: [AppService],
