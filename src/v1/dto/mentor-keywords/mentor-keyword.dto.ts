49c218e5b7d2075ddf85445ce3d5325c80b0a907
nakkim <kdkeiie8@gmail.com>
2022-08-04 14:54:26 +0900

    Delete DTOs

--- src/v1/dto/mentor-keywords/mentor-keyword.dto.ts
+++
@@ -1,8 +0,0 @@
-import { MentorDto } from '../mentors/mentor.dto';
-
-export interface MentorKeywordDto {
-  mentorId: string;
-  keywordId: string;
-  keywords: MentorKeywordDto;
-  mentors: MentorDto;
-}
