aa2e1b4cf9cf3277f408ffe55a67f08e0da7004c
nakkim <kdkeiie8@gmail.com>
2022-08-04 14:55:16 +0900

    Feat: 카뎃 멘토링 기록 검색 구현
    GET /api/v1/cadets/mentorings -> 카뎃의 멘토링 기록 리턴

--- src/v1/cadets/cadets.controller.ts
+++ src/v1/cadets/cadets.controller.ts
@@ -2,14 +2,25 @@ import { Controller, Get, Req, UseGuards } from '@nestjs/common';
 import { Roles } from 'src/v1/decorators/roles.decorator';
 import { JwtGuard } from '../guards/jwt.guard';
 import { RolesGuard } from '../guards/role.guard';
+import { CadetsService } from './service/cadets.service';
 
 @Controller()
 export class CadetsController {
+  constructor(private cadetsService: CadetsService) {}
+
   @Get('test')
   @Roles('cadet')
   @UseGuards(JwtGuard, RolesGuard)
   hello(@Req() req) {
     console.log('guard test', req.user);
     return 'hi';
+  }
+
+  @Get('mentorings')
+  @Roles('cadet')
+  @UseGuards(JwtGuard, RolesGuard)
+  async getMentoringLogs(@Req() req) {
+    const logs = await this.cadetsService.getMentoringLogs(req.user.id);
+    return logs;
   }
 }
--- src/v1/cadets/service/cadets.service.ts
+++ src/v1/cadets/service/cadets.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { CreateCadetDto } from 'src/v1/dto/cadets/create-cadet.dto';
 import { Cadets } from 'src/v1/entities/cadets.entity';
+import { MentoringLogs } from 'src/v1/entities/mentoring-logs.entity';
 import { Repository } from 'typeorm';
 
 @Injectable()
@@ -19,5 +20,13 @@ export class CadetsService {
   async findByIntra(intraId: string) {
     const foundUser = await this.cadetsRepository.findOneBy({ intraId });
     return { id: foundUser?.id, intraId: foundUser?.intraId, role: 'cadet' };
+  }
+
+  async getMentoringLogs(id: string): Promise<MentoringLogs[]> {
+    const cadet: Cadets = await this.cadetsRepository.findOne({
+      where: { id },
+      relations: { mentoringLogs: true },
+    });
+    return cadet.mentoringLogs;
   }
 }
