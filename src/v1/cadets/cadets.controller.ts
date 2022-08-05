515bb2c4338f2fd48f86ecd40436b1363c40d79e
dkjefilsjl <6792kjs@naver.com>
2022-08-05 11:46:40 +0900

    add apply last

--- src/v1/cadets/cadets.controller.ts
+++ src/v1/cadets/cadets.controller.ts
@@ -1,7 +1,8 @@
-import { Controller, Get, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, UseGuards } from '@nestjs/common';
 import { Roles } from 'src/v1/decorators/roles.decorator';
 import { User } from '../decorators/user.decorator';
 import { CadetMentoringInfo } from '../dto/cadet-mentoring-info.interface';
+import { CreateApplyDto } from '../dto/cadets/create-apply.dto';
 import { jwtUser } from '../dto/jwt-user.interface';
 import { JwtGuard } from '../guards/jwt.guard';
 import { RolesGuard } from '../guards/role.guard';
@@ -9,6 +10,7 @@ import { CadetsService } from './service/cadets.service';
 
 @Controller()
 export class CadetsController {
+  applyService: any;
   constructor(private cadetsService: CadetsService) {}
 
   @Get('test')
@@ -24,5 +26,16 @@ export class CadetsController {
   @UseGuards(JwtGuard, RolesGuard)
   async getMentoringLogs(@User() user: jwtUser): Promise<CadetMentoringInfo> {
     return await this.cadetsService.getMentoringLogs(user.id);
+  }
+
+  @Roles('cadet')
+  @UseGuards(JwtGuard, RolesGuard)
+  @Post('mentorings/apply/:mentorId')
+  create(
+    @Param('mentorId') mentorId: string,
+    @User() user: jwtUser,
+    @Body() createApplyDto: CreateApplyDto,
+  ) {
+    return this.applyService.create(user, mentorId, createApplyDto);
   }
 }
