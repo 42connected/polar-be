6f77294cc5b284955082e3ed8ff491ab15be5ac7
dkjefilsjl <6792kjs@naver.com>
2022-08-05 19:24:50 +0900

    fix: dto/create-apply

--- src/v1/dto/cadets/create-apply.dto.ts
+++ src/v1/dto/cadets/create-apply.dto.ts
@@ -1,8 +1,24 @@
-export interface CreateApplyDto {
+import { Type } from 'class-transformer';
+import { IsNotEmpty, IsOptional, IsString } from 'class-validator';
+
+export class CreateApplyDto {
+  @IsString()
+  @IsNotEmpty()
   topic: string;
 
+  @IsString()
+  @IsNotEmpty()
   content: string;
+
+  @Type(() => Date)
+  @IsNotEmpty()
   requestTime1: Date[];
+
+  @IsOptional()
+  @Type(() => Date)
   requestTime2: Date[];
+
+  @IsOptional()
+  @Type(() => Date)
   requestTime3: Date[];
 }
