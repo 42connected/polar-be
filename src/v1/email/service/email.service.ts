e76d4461a9273b99e9417932ce7a74c60aeda147
jeounpar <cdex6531@ajou.ac.kr>
2022-08-09 23:19:07 +0900

    Feat: 이메일 보내기 기능 추가중

--- src/v1/email/service/email.service.ts
+++ src/v1/email/service/email.service.ts
@@ -1,20 +1,51 @@
 import { MailerService } from '@nestjs-modules/mailer';
 import { Injectable } from '@nestjs/common';
+import { ReservationMessageDto } from 'src/v1/dto/slack/send-message.dto';
 
 @Injectable()
 export class EmailService {
   constructor(private mailService: MailerService) {}
-  async sendEmail() {
+  async sendEmail(reservationMessageDto: ReservationMessageDto) {
+    const {
+      mentorSlackId,
+      cadetSlackId,
+      reservationTime,
+      mentoringTime,
+      isCommon,
+    } = reservationMessageDto;
+    const reservationTimeTmp: string = reservationTime.toDateString();
+    const tmp = reservationTimeTmp.split(' ');
+    const reservationTimeToString =
+      tmp[1] +
+      ' ' +
+      tmp[2] +
+      ', ' +
+      tmp[3] +
+      ' ' +
+      reservationTime.getHours() +
+      ':' +
+      reservationTime.getMinutes() +
+      ' for ' +
+      mentoringTime +
+      ' hours';
+    let commonType: string;
+    if (isCommon) {
+      commonType = '공통과정';
+    } else {
+      commonType = '심화과정';
+    }
+		try{
     const response = await this.mailService.sendMail({
       to: 'cdex6531@gmail.com',
       from: '42polar-no-reply@gmail.com',
       subject: 'Test',
-      template: 'send-email.hbs',
+      template: 'ReservationMessage.hbs',
       context: {
-        name: 'park',
+        username: 'park',
         code: '1234',
       },
     });
+	} catch {}
     return response;
   }
 }
