c818ec10078a0bc24f6162b5094b2af6558e60bf
nakkim <kdkeiie8@gmail.com>
2022-08-09 17:14:04 +0900

    Feat: REDIS DB 연결

--- src/bull-queue/bull-queue.module.ts
+++ src/bull-queue/bull-queue.module.ts
@@ -2,14 +2,16 @@ import { BullModule } from '@nestjs/bull';
 import { Global, Module } from '@nestjs/common';
 import { LoginConsumer } from './login-consumer';
 import { LoginProducer } from './login-producer';
+import 'dotenv/config';
 
 @Global()
 @Module({
   imports: [
     BullModule.forRoot({
       redis: {
-        host: 'localhost',
-        port: 6379,
+        host: process.env.REDIS_HOST,
+        port: +process.env.REDIS_PORT,
+        password: process.env.REDIS_PASSWORD,
       },
       defaultJobOptions: {
         removeOnComplete: true,
