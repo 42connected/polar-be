03ab6b29a625b6768b5fb9e4f306204c02b963be
nakkim <kdkeiie8@gmail.com>
2022-08-10 19:00:46 +0900

    Feat: Create Mentors E2E Test

---
+++ test/mentors.e2e-spec.ts
@@ -0,0 +1,20 @@
+import { INestApplication, ValidationPipe } from '@nestjs/common';
+import { Test, TestingModule } from '@nestjs/testing';
+
+describe('MentorsController (e2e)', () => {
+  let app: INestApplication;
+
+  beforeEach(async () => {
+    const moduleFixture: TestingModule = await Test.createTestingModule({
+      imports: [],
+    }).compile();
+
+    app = moduleFixture.createNestApplication();
+    app.useGlobalPipes(new ValidationPipe());
+    await app.init();
+  });
+
+  afterAll(async () => {
+    await app.close();
+  });
+});
