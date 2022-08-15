91154fcec208627f52b4952098d6d4c0c2f093e3
nakkim <kdkeiie8@gmail.com>
2022-08-15 15:51:35 +0900

    Feat: 멘토링 완료 기능 추가, 상태 변경 메서드 하나로 합침

--- src/v1/mentoring-logs/mentoring-logs.controller.ts
+++ src/v1/mentoring-logs/mentoring-logs.controller.ts
@@ -3,12 +3,16 @@ import { ApiBearerAuth, ApiOperation } from '@nestjs/swagger';
 import { Roles } from '../decorators/roles.decorator';
 import { User } from '../decorators/user.decorator';
 import { ApproveMentoringDto } from '../dto/mentoring-logs/approve-mentoring.dto';
+import { CompleteMentoringDto } from '../dto/mentoring-logs/complete-mentoring.dto';
 import { RejectMentoringDto } from '../dto/mentoring-logs/reject-mentoring.dto';
 import { EmailService, MailType } from '../email/service/email.service';
 import { JwtGuard } from '../guards/jwt.guard';
 import { RolesGuard } from '../guards/role.guard';
 import { JwtUser } from '../interface/jwt-user.interface';
-import { MentoringLogsService } from './service/mentoring-logs.service';
+import {
+  MentoringLogsService,
+  MentoringLogStatus,
+} from './service/mentoring-logs.service';
 
 @Controller()
 export class MentoringLogsController {
@@ -29,7 +33,12 @@ export class MentoringLogsController {
     @User() user: JwtUser,
     @Body() body: ApproveMentoringDto,
   ) {
-    const log = await this.mentoringLogsService.approve(body, user.intraId);
+    const log = await this.mentoringLogsService.changeStatus(
+      user.intraId,
+      body.mentoringLogId,
+      MentoringLogStatus.Approve,
+      body.meetingAt,
+    );
     this.emailService.sendMessage(log.id, MailType.Approve);
   }
 
@@ -45,7 +54,32 @@ export class MentoringLogsController {
     @User() user: JwtUser,
     @Body() body: RejectMentoringDto,
   ) {
-    const log = await this.mentoringLogsService.cancel(body, user.intraId);
+    const log = await this.mentoringLogsService.changeStatus(
+      user.intraId,
+      body.mentoringLogId,
+      MentoringLogStatus.Cancel,
+      null,
+      body.rejectMessage,
+    );
     this.emailService.sendMessage(log.id, MailType.Cancel);
+  }
+
+  @Patch('done')
+  @Roles('mentor')
+  @UseGuards(JwtGuard, RolesGuard)
+  @ApiBearerAuth('access-token')
+  @ApiOperation({
+    summary: 'Mentoring Complete Processing API',
+    description: '멘토링 완료 처리 API',
+  })
+  async CompleteMentoring(
+    @User() user: JwtUser,
+    @Body() body: CompleteMentoringDto,
+  ) {
+    const log = await this.mentoringLogsService.changeStatus(
+      user.intraId,
+      body.mentoringLogId,
+      MentoringLogStatus.Done,
+    );
   }
 }
--- src/v1/mentoring-logs/service/mentoring-logs.service.ts
+++ src/v1/mentoring-logs/service/mentoring-logs.service.ts
@@ -1,4 +1,5 @@
 import {
+  BadRequestException,
   ConflictException,
   HttpException,
   HttpStatus,
@@ -6,8 +7,6 @@ import {
 } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { ApplyService } from 'src/v1/cadets/apply/apply.service';
-import { ApproveMentoringDto } from 'src/v1/dto/mentoring-logs/approve-mentoring.dto';
-import { RejectMentoringDto } from 'src/v1/dto/mentoring-logs/reject-mentoring.dto';
 import { MentoringLogs } from 'src/v1/entities/mentoring-logs.entity';
 import { Repository } from 'typeorm';
 
@@ -45,53 +44,91 @@ export class MentoringLogsService {
     }
   }
 
-  async approve(
-    approveInfos: ApproveMentoringDto,
+  validateUser(
     userId: string,
-  ): Promise<MentoringLogs> {
-    const { mentoringLogId, meetingAt } = approveInfos;
-    const foundLog: MentoringLogs = await this.findMentoringLogWithRelations(
-      mentoringLogId,
-    );
-    if (userId !== foundLog.mentors.intraId) {
-      throw new HttpException(
-        {
-          status: HttpStatus.FORBIDDEN,
-          error: '멘토링 로그에 대한 처리 권한이 없습니다.',
-        },
-        HttpStatus.FORBIDDEN,
-      );
+    status: MentoringLogStatus,
+    foundLog: MentoringLogs,
+  ): void {
+    if (status === MentoringLogStatus.Cancel) {
+      if (
+        userId !== foundLog.mentors.intraId &&
+        userId !== foundLog.cadets.intraId
+      ) {
+        throw new HttpException(
+          {
+            status: HttpStatus.FORBIDDEN,
+            error: '멘토링 로그에 대한 처리 권한이 없습니다.',
+          },
+          HttpStatus.FORBIDDEN,
+        );
+      }
+    } else {
+      if (userId !== foundLog.mentors.intraId) {
+        throw new HttpException(
+          {
+            status: HttpStatus.FORBIDDEN,
+            error: '멘토링 로그에 대한 처리 권한이 없습니다.',
+          },
+          HttpStatus.FORBIDDEN,
+        );
+      }
     }
-    this.applyService.checkDate(meetingAt[0], meetingAt[1]);
-    foundLog.status = MentoringLogStatus.Approve;
-    foundLog.meetingAt = meetingAt;
-    try {
-      await this.mentoringLogsRepository.save(foundLog);
-    } catch (err) {
-      throw new ConflictException('멘토링 로그 저장 중 에러가 발생했습니다.');
-    }
-    return foundLog;
   }
 
-  async cancel(rejectInfos: RejectMentoringDto, userId: string) {
-    const { mentoringLogId, rejectMessage } = rejectInfos;
+  validateStatus(
+    currentStatus: string,
+    futureStatus: MentoringLogStatus,
+  ): void {
+    switch (futureStatus) {
+      case MentoringLogStatus.Approve:
+        if (currentStatus !== MentoringLogStatus.Wait) {
+          throw new BadRequestException(
+            '대기중 상태인 멘토링 로그만 확정이 가능합니다.',
+          );
+        }
+        break;
+      case MentoringLogStatus.Cancel:
+        if (
+          currentStatus !== MentoringLogStatus.Wait &&
+          currentStatus !== MentoringLogStatus.Approve
+        ) {
+          throw new BadRequestException(
+            '대기중이거나 확정된 멘토링만 취소가 가능합니다.',
+          );
+        }
+        break;
+      case MentoringLogStatus.Done:
+        if (currentStatus !== MentoringLogStatus.Approve) {
+          throw new BadRequestException('확정된 멘토링만 완료가 가능합니다.');
+        }
+        break;
+      case MentoringLogStatus.Wait:
+        throw new BadRequestException(
+          '멘토링 상태를 대기중으로 변경이 불가능합니다.',
+        );
+    }
+  }
+
+  async changeStatus(
+    userId: string,
+    mentoringLogId: string,
+    status: MentoringLogStatus,
+    meetingAt?: Date[],
+    rejectMessage?: string,
+  ) {
     const foundLog: MentoringLogs = await this.findMentoringLogWithRelations(
       mentoringLogId,
     );
-    if (
-      userId !== foundLog.mentors.intraId &&
-      userId !== foundLog.cadets.intraId
-    ) {
-      throw new HttpException(
-        {
-          status: HttpStatus.FORBIDDEN,
-          error: '멘토링 로그에 대한 처리 권한이 없습니다.',
-        },
-        HttpStatus.FORBIDDEN,
-      );
+    this.validateUser(userId, status, foundLog);
+    this.validateStatus(foundLog.status, status);
+    foundLog.status = status;
+    if (status === MentoringLogStatus.Cancel) {
+      foundLog.rejectMessage = rejectMessage;
     }
-    foundLog.status = MentoringLogStatus.Cancel;
-    foundLog.rejectMessage = rejectMessage;
+    if (status === MentoringLogStatus.Approve) {
+      this.applyService.checkDate(meetingAt[0], meetingAt[1]);
+      foundLog.meetingAt = meetingAt;
+    }
     try {
       await this.mentoringLogsRepository.save(foundLog);
     } catch (err) {
