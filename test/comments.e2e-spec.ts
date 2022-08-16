a2080840bd88ab352c013c7caa10ad864c783f23
nakkim <kdkeiie8@gmail.com>
2022-08-16 15:17:26 +0900

    Test: Comments E2E

--- test/comments.e2e-spec.ts
+++ test/comments.e2e-spec.ts
@@ -3,24 +3,32 @@ import {
   INestApplication,
   ValidationPipe,
 } from '@nestjs/common';
-import * as request from 'supertest';
 import { JwtModule } from '@nestjs/jwt';
 import { Test, TestingModule } from '@nestjs/testing';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import * as request from 'supertest';
+import { Repository } from 'typeorm';
 import { JwtGuard } from 'src/v1/guards/jwt.guard';
 import { JwtStrategy } from 'src/v1/strategies/jwt.strategy';
+import { ConfigModule } from '@nestjs/config';
+import { CommentsModule } from 'src/v1/comments/comments.module';
+import { AuthModule } from 'src/v1/auth/auth.module';
+import { BullQueueModule } from 'src/bull-queue/bull-queue.module';
+import { CreateCommentDto } from 'src/v1/dto/comment/comment.dto';
 import { SnakeNamingStrategy } from 'typeorm-naming-strategies';
-import { Repository } from 'typeorm';
-import { Mentors } from 'src/v1/entities/mentors.entity';
+import { Comments } from 'src/v1/entities/comments.entity';
 
-describe('CommentsController', () => {
+describe('CommentsController (e2e)', () => {
   let app: INestApplication;
-  let mentorsRepo: Repository<Mentors>;
+  let commentsRepo: Repository<Comments>;
   const mentorIntraId = 'm-dada';
 
   beforeAll(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [
+        ConfigModule.forRoot({
+          isGlobal: true,
+        }),
         TypeOrmModule.forRoot({
           type: 'postgres',
           host: process.env.POSTGRES_HOST,
@@ -41,6 +49,9 @@ describe('CommentsController', () => {
             };
           },
         }),
+        AuthModule,
+        BullQueueModule,
+        CommentsModule,
       ],
       providers: [JwtStrategy],
     })
@@ -48,14 +59,15 @@ describe('CommentsController', () => {
       .useValue({
         canActivate: (context: ExecutionContext) => {
           const req = context.switchToHttp().getRequest();
-          req.user = { intraId: 'jojoo', role: 'cadet' };
+          req.user = { intraId: 'nakkim', role: 'cadet' };
           return true;
         },
       })
       .compile();
 
     app = moduleFixture.createNestApplication();
-    mentorsRepo = moduleFixture.get<Repository<Mentors>>('MentorsRepository');
+    commentsRepo =
+      moduleFixture.get<Repository<Comments>>('CommentsRepository');
     app.useGlobalPipes(
       new ValidationPipe({
         whitelist: true,
@@ -73,6 +85,24 @@ describe('CommentsController', () => {
     return request(app.getHttpServer())
       .get(`/${mentorIntraId}?take=10&page=1`)
       .expect(200);
+  });
+
+  it('POST /:mentorIntraId', () => {
+    const body: CreateCommentDto = {
+      content: '테스트',
+    };
+    return request(app.getHttpServer())
+      .post(`/${mentorIntraId}`)
+      .send(body)
+      .expect(201);
+  });
+
+  // it('PATCH /:commentId', () => {
+  // })
+
+  it('DELETE /:commentId', async () => {
+    const target = await commentsRepo.findOneBy({ content: '테스트' });
+    return request(app.getHttpServer()).delete(`/${target.id}`).expect(200);
   });
 
   afterAll(async () => {
