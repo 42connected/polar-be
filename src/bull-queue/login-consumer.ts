6d044012738c89857c92a707610585aab2b22497
nakkim <kdkeiie8@gmail.com>
2022-08-22 18:53:44 +0900

    Remove: console.log

--- src/bull-queue/login-consumer.ts
+++ src/bull-queue/login-consumer.ts
@@ -8,7 +8,6 @@ import { LoginJob } from '../v1/interface/login-job.interface';
 export class LoginConsumer {
   @Process('get-profile')
   async login(job: Job<LoginJob>, done) {
-    console.log(`Processing Job(${job.id})`);
     const { accessToken, url } = job.data;
     let res;
     try {
@@ -21,7 +20,6 @@ export class LoginConsumer {
     } catch (err) {
       throw new ConflictException(err, 'fetch 작업 중 에러가 발생했습니다.');
     }
-    console.log(res.status);
     if (res.status !== 200) {
       throw new UnauthorizedException('프로필 정보를 받아올 수 없습니다.');
     }
