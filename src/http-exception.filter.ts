39e52dd0466e8ae3495cbead140b62652c7d4a27
nakkim <kdkeiie8@gmail.com>
2022-08-23 15:49:54 +0900

    Feat: 글로벌 예외 필터 - 로거 수정

--- src/http-exception.filter.ts
+++ src/http-exception.filter.ts
@@ -28,16 +28,19 @@ export class AllExceptionsFilter implements ExceptionFilter {
         path: httpAdapter.getRequestUrl(ctx.getRequest()),
         message: exception.message,
       };
+      Logger.error(
+        `${request.method} ${responseBody.path} ${responseBody.statusCode} | ${responseBody.message}`,
+      );
     } else {
       responseBody = {
         statusCode: HttpStatus.INTERNAL_SERVER_ERROR,
         timestamp: new Date().toISOString(),
         path: httpAdapter.getRequestUrl(ctx.getRequest()),
       };
+      Logger.error(
+        `${request.method} ${responseBody.path} ${responseBody.statusCode} `,
+      );
     }
-    Logger.error(
-      `${request.method} ${responseBody.path} ${responseBody.statusCode} `,
-    );
     httpAdapter.reply(ctx.getResponse(), responseBody, responseBody.statusCode);
   }
 }
