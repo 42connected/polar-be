6aca108ca209a55679f61aaa5766fa3a3a2fd03b
nakkim <kdkeiie8@gmail.com>
2022-08-26 10:46:51 +0900

    Feat: 레포트 페이지네이션 DTO 생성

--- src/v1/bocals/service/data-room.service.ts
+++ src/v1/bocals/service/data-room.service.ts
@@ -3,6 +3,7 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { GetDataRoomDto } from 'src/v1/dto/bocals/get-data-room.dto';
 import { Reports } from 'src/v1/entities/reports.entity';
 import { Repository } from 'typeorm';
+import { PaginationReportDto } from '../../dto/reports/pagination-report.dto';
 
 @Injectable()
 export class DataRoomService {
@@ -10,56 +11,64 @@ export class DataRoomService {
     @InjectRepository(Reports)
     private readonly reportsRepository: Repository<Reports>,
   ) {}
-  async getReportPagination(
-    getDataRoomDto: GetDataRoomDto,
+
+  async findAndCountReports(
+    pagination: GetDataRoomDto,
   ): Promise<[Reports[], number]> {
     try {
-      const reports: [Reports[], number] =
-        await this.reportsRepository.findAndCount({
-          where: {
-            mentors: {
-              intraId: getDataRoomDto.mentorIntra,
-              name: getDataRoomDto.mentorName,
-            },
+      return await this.reportsRepository.findAndCount({
+        where: {
+          mentors: {
+            intraId: pagination.mentorIntra,
+            name: pagination.mentorName,
           },
-          relations: {
-            mentoringLogs: true,
-            cadets: true,
-            mentors: true,
-          },
-          select: {
+        },
+        relations: {
+          mentoringLogs: true,
+          cadets: true,
+          mentors: true,
+        },
+        select: {
+          id: true,
+          place: true,
+          createdAt: true,
+          signatureUrl: true,
+          imageUrl: true,
+          money: true,
+          status: true,
+          mentoringLogs: {
             id: true,
-            place: true,
             createdAt: true,
-            signatureUrl: true,
-            imageUrl: true,
-            money: true,
-            status: true,
-            mentoringLogs: {
-              id: true,
-              createdAt: true,
-              meetingAt: true,
-            },
-            mentors: {
-              intraId: true,
-              name: true,
-              duty: true,
-            },
-            cadets: {
-              intraId: true,
-              name: true,
-              isCommon: true,
-            },
+            meetingAt: true,
           },
-          order: {
-            createdAt: getDataRoomDto.isAscending ? 'ASC' : 'DESC',
+          mentors: {
+            intraId: true,
+            name: true,
+            duty: true,
           },
-          skip: getDataRoomDto.take * (getDataRoomDto.page - 1),
-          take: getDataRoomDto.take,
-        });
-      return reports;
+          cadets: {
+            intraId: true,
+            name: true,
+            isCommon: true,
+          },
+        },
+        order: {
+          createdAt: pagination.isAscending ? 'ASC' : 'DESC',
+        },
+        skip: pagination.take * (pagination.page - 1),
+        take: pagination.take,
+      });
     } catch (e) {
       throw new ConflictException('예기치 못한 에러가 발생하였습니다');
     }
+  }
+
+  async getReportPagination(
+    getDataRoomDto: GetDataRoomDto,
+  ): Promise<PaginationReportDto> {
+    const result: [Reports[], number] = await this.findAndCountReports(
+      getDataRoomDto,
+    );
+    return { reports: result[0], total: result[1] };
   }
 }
