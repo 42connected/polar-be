b88b2606447549b3583c16a6f65aa70e5a8b2d9b
kanghyki <kanghyki@gmail.com>
2022-09-10 23:51:06 +0900

    Fix: manageMentoringLogs.. .getTime()

--- src/v1/batch/batch.service.ts
+++ src/v1/batch/batch.service.ts
@@ -195,11 +195,20 @@ export class BatchService {
 
     mentoringLogs
       .filter(e => {
-        if (e?.requestTime1?.[startIndex] && e.requestTime1[startIndex] < now)
+        if (
+          e?.requestTime1?.[startIndex] &&
+          e.requestTime1[startIndex].getTime() < now.getTime()
+        )
           return true;
-        if (e?.requestTime2?.[startIndex] && e.requestTime2[startIndex] < now)
+        if (
+          e?.requestTime2?.[startIndex] &&
+          e.requestTime2[startIndex].getTime() < now.getTime()
+        )
           return true;
-        if (e?.requestTime3?.[startIndex] && e.requestTime3[startIndex] < now)
+        if (
+          e?.requestTime3?.[startIndex] &&
+          e.requestTime3[startIndex].getTime() < now.getTime()
+        )
           return true;
         return false;
       })
