edc1aac0c2cd3c7b56b234a46661bd5d976f488e
nakkim <kdkeiie8@gmail.com>
2022-10-05 01:13:09 +0900

    feat: 카테고리 캐싱 적용
    카테고리-키워드 데이터는 변할 일이 거의 없어서 한시간으로 설정
    멘토 검색 결과는 1분으로 지정함(멘토 활동 상태나 자기소개 변경 적용 위함)

--- src/v1/categories/categories.module.ts
+++ src/v1/categories/categories.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import { CacheInterceptor, CacheModule, Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from '../auth/auth.module';
 import { Categories } from '../entities/categories.entity';
@@ -11,9 +11,17 @@ import { CategoriesController } from './categories.controller';
 import { CategoriesService } from './service/categories.service';
 import { KeywordsService } from './service/keywords.service';
 import { SearchMentorsService } from './service/search-mentors.service';
+import * as redisStore from 'cache-manager-redis-store';
+import { APP_INTERCEPTOR } from '@nestjs/core';
 
 @Module({
   imports: [
+    CacheModule.register({
+      store: redisStore,
+      host: 'localhost',
+      port: 6379,
+      ttl: 60 * 60,
+    }),
     TypeOrmModule.forFeature([
       Categories,
       Mentors,
@@ -25,6 +33,14 @@ import { SearchMentorsService } from './service/search-mentors.service';
     AuthModule,
   ],
   controllers: [CategoriesController],
-  providers: [CategoriesService, SearchMentorsService, KeywordsService],
+  providers: [
+    CategoriesService,
+    SearchMentorsService,
+    KeywordsService,
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: CacheInterceptor,
+    },
+  ],
 })
 export class CategoriesModule {}
