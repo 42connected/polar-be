ae5b4d2b88da3bf1e03beb7c9d641d9e6f6f1c16
kanghyki <kanghyki@gmail.com>
2022-11-09 11:45:40 +0900

    style(mentor-keywords.interface): 안쓰는 임포트 삭제

--- src/v1/interface/mentor-keywords/mentor-keywords.interface.ts
+++ src/v1/interface/mentor-keywords/mentor-keywords.interface.ts
@@ -1,6 +1,3 @@
-import { KeywordsInterface } from '../keywords/keywords.interface';
-import { MentorsInterface } from '../mentors/mentors.interface';
-
 export interface MentorKeywordsInterface {
   mentorId: string;
   keywordId: string;
